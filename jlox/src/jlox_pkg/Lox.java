5374681a7ae73b3b04e148fcf8dd74cf6a421c78
aradiuk <aradiuk@bitbucket-cableos.harmonicinc.com>
2022-05-23 14:53:05 +0300

    Finish interpreter base

--- jlox/src/jlox_pkg/Lox.java
+++ jlox/src/jlox_pkg/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -29,6 +31,9 @@ public class Lox {
 
         if (hadError) {
             System.exit(65);
+        }
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -57,7 +62,7 @@ public class Lox {
             return;
         }
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     private static void report(int line, String where, String message) {
@@ -75,5 +80,10 @@ public class Lox {
         } else {
             report(token.line, " at " + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
